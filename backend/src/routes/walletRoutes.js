d4505f2827750b132c9f29748e96aaf0c6ef67c1
Lohitha Asish <asishkumarv@github.com>
2026-06-01 13:42:22 +0530

    error solved

--- backend/src/routes/walletRoutes.js
+++ backend/src/routes/walletRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getWalletStats, requestWithdrawal } = require('../controllers/walletController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/auth');
 
 router.get('/', protect, getWalletStats);
 router.post('/withdraw', protect, requestWithdrawal);
