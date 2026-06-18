7b51c308e820ef9ab4f0e5ece8ef9a8865aa6a65
Lohitha Asish <asishkumarv@github.com>
2026-06-18 15:15:54 +0530

    notification error update

--- backend/src/routes/notificationRoutes.js
+++ backend/src/routes/notificationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authenticateUser } = require('../middleware/authMiddleware');
+const { authenticateUser } = require('../middleware/auth');
 const { getNotifications, markAsRead, markAllAsRead } = require('../controllers/notificationController');
 
 router.get('/', authenticateUser, getNotifications);
